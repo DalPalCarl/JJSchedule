a91ac88a27afa993edd8426633add44676fe6f8a
Dallas <dallas.carlson10@gmail.com>
2024-11-03 00:48:24 -0500

    Add form submission information functionality

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -3,21 +3,52 @@ import { useState } from 'react';
 const SignIn = () => {
     const [isSignIn, setIsSignIn] = useState(true);
 
+    const submitSignIn = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const load = Object.fromEntries(formData);
+
+        const entryData = {
+            userId: load.employeeId,
+            pw: load.employeePw
+        }
+        console.log(entryData);
+    }
+
+    const submitCreateUser = (e) => {
+        e.preventDefault();
+
+    }
+
     return(
-        <div className="my-2 mx-auto w-50 text-start">
-            <h1>{isSignIn ? "Sign In" : "Create Account"}</h1>
-            <form>
-                <label for="employeeId" >Employee ID</label>
-                <input type='text' id='employeeId' className='form-control'/>
-                <label for="employeePw">Password</label>
-                <input type='password' id='employeePw' className='form-control' />
-                {!isSignIn ? 
-                    <>
-                        <label for='confirmPw'>Confirm Password</label>
-                        <input type="password" id='confirmPw' className='form-control'/>
-                    </> : <></>
-                    }
-            </form> 
+        <div className="my-2 mx-auto w-50 text-start my-2">
+            {isSignIn ? 
+            <>
+                <h1>Sign In</h1>
+                <form onSubmit={submitSignIn} className='justify-content-start' style={{width: 300}}>
+                    <label for="employeeId" className='my-2'>Employee ID</label>
+                    <input type='text' id='employeeId' name='employeeId' className='form-control' maxLength={4}/>
+                    <label for="employeePw" className='my-2'>Password</label>
+                    <input type='password' id='employeePw' name='employeePw' className='form-control' />
+                    <button type='submit' className='btn btn-primary my-2'>Sign In</button>
+                    {/* <button className='btn btn-secondary my-2 mx-2' onClick={() => {setIsSignIn(!isSignIn)}}>Create User</button> */}
+                </form> 
+            </> :
+            <>
+                <h1>Create User</h1>
+                <form onSubmit={submitCreateUser} className='justify-content-start' style={{width: 300}}>
+                    <label for="employeeId" className='my-2'>Employee ID</label>
+                    <input type='text' id='employeeId' className='form-control' maxLength={4}/>
+                    <label for="employeePw" className='my-2'>Password</label>
+                    <input type='password' id='employeePw' className='form-control' />
+                    <label for='confirmPw' className='my-2'>Confirm Password</label>
+                    <input type="password" id='confirmPw' className='form-control'/>
+                    <button type='submit' className='btn btn-primary my-2'>Create User</button>
+                    <button className='btn btn-secondary m-2' onClick={() => {setIsSignIn(!isSignIn)}}>Sign In</button>
+                </form>
+            </>  
+            }
+            
         </div>
     );
 }
