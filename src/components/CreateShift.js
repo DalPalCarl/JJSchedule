787cd00483984a936bde5dccf9be8864b11b3c48
Dallas <dallas.carlson10@gmail.com>
2024-11-04 12:38:07 -0600

    Implement some code for entering shift information

--- src/components/CreateShift.js
+++ src/components/CreateShift.js
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.module.css';
 
@@ -7,6 +7,11 @@ const CreateShift = () => {
     const [pickedDate, setPickedDate] = useState(new Date());
     const [employeeList, setEmployeeList] = useState([]);
     const [employee, setEmployee] = useState({});
+    const [startShiftHH, setStartShiftHH] = useState("");
+    const [startShiftMM, setStartShiftMM] = useState("");
+    const [endShiftHH, setEndShiftHH] = useState("");
+    const [endShiftMM, setEndShiftMM] = useState("");
+
 
     useEffect(() => {
         fetch("http://localhost:8080/users")
@@ -18,27 +23,39 @@ const CreateShift = () => {
     }, [])
 
     return(
-        <div className='text-start mx-auto m-3' style={{width: 400}}>
-            <h1>Create Shift</h1>
-            <div className='input-group'>
+        <div className='mx-auto m-3 col text-start' style={{width: 500}}>
+            <h1 className='my-3'>Create Shift</h1>
+            <label htmlFor='employeeSelect'>Select Employee</label>
+            <div className='input-group mb-5' id='employeeSelect'>
                 <button type='button' className='btn btn-secondary dropdown-toggle' data-bs-toggle="dropdown" data-bs-auto-close='true' aria-expanded='false'>
                     Select Employee
                 </button>
                 <ul className='dropdown-menu'>
-                    {employeeList.map((employee) => {
+                    {employeeList.map((employee, i) => {
                         return(
-                            <li className='dropdown-item' onClick={() => setEmployee(employee)}>{employee.fname}</li>
+                            <li key={employee + "" + i} className='dropdown-item' onClick={() => setEmployee(employee)}>{employee.fname}</li>
                         )
                     })}
                 </ul>
                 <input type="text" className='form-control' placeholder='Employee' value={employee?.fname ? employee.fname : "Select Employee"} readOnly />
             </div>
-            <DatePicker selected={pickedDate} 
+            <label htmlFor='dateSelect'>Select Date</label>
+            <br/>
+            <DatePicker id="dateSelect" selected={pickedDate} 
                 onChange={(date) => {
                     setPickedDate(date);
                     console.log("Selected Date: ", date);
                 }
             }/>
+            <div className='row'>
+            <label htmlFor='startTime'>Start</label>
+                <div className='input-group'>
+                    <input type='number' id='startTime' placeholder='HH' min="1" max="12" onChange={(c) => setStartShiftHH(c)} />
+                    <input type='number' placeholder='MM' min="0" max="45" step="15" onChange={(c) => setStartShiftMM(c)} />
+                </div>
+                
+            </div>
+
         </div>
     )
 }
